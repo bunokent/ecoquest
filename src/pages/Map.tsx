bf821cbc999ad80e1d13403a89c1557b2dd4d039
psyche-ee <eduardojr.belda@ctu.edu.ph>
2026-02-01 01:23:07 +0800

    add animation for map play button

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -107,7 +107,7 @@ const Map = () => {
           className="relative flex items-center justify-center"
           onClick={() => setPlay(true)}
         >
-          <img src={PlayBtn} aria-hidden={true} className="h-24 w-auto" />
+          <img src={PlayBtn} aria-hidden={true} className="h-24 w-auto npc-float" />
           <span className="absolute tracking-wide"></span>
         </button>
       </div>
